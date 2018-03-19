be486226740d23013e624e76e6a4bdc8b1b80979
s-115-30 <s-115-30@gumiforest.ae.krakow.pl>
2018-03-19 16:13:31 +0100

    JQMobile

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -9,12 +9,4 @@ function onDeviceReady() {
 
 function deviceInfo() {
 
-	info =  'Device Model   : '    + device.model + '<br>' + 
-			'Device Name    : '     + device.name + '<br>' + 
-			'Device Cordova : '  + device.cordova + '<br>' + 
-			'Device Platform: ' + device.platform + '<br>' + 
-			'Device UUID    : '     + device.uuid + '<br>' + 
-			'Device Version : '  + device.version + '<br>';
-
-	document.getElementById("deviceDetails").innerHTML = info;	
 }
